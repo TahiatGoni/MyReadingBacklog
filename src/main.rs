71c614cbae38ebb5d2948cb6cc1155a2f460122c
Tahiat <tahiatg@gmail.com>
2024-11-10 18:01:27 -0700

    added user stats tracking and displaying

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ fn print_help() {
             --get_queue_article
             --get_random_article
             --article_read
+            --stats
         "
     )
 }
@@ -21,32 +22,37 @@ fn main() -> Result<()> {
         Err(err) => eprintln!("{}", err),
     };
 
+    let mut stats = match storage::user_stats() {
+        Ok(res) => res,
+        Err(err) => panic!("{}", err),
+    };
+
     //implemented arg --add and get for now
     let args: Vec<String> = env::args().collect();
     if args.len() == 1 {
         print_help();
     } else {
         if args[1] == "--add" {
-            match storage::add_item(&mut conn, &args[2]) {
+            match storage::add_item(&mut conn, &args[2], &mut stats) {
                 Ok(_res) => println!("ok"),
                 Err(err) => eprintln!("{}", err),
             };
         } else if args[1] == "--get_queue_article" {
-            match storage::get_article_fifo(&conn) {
+            match storage::get_article_fifo(&conn, &mut stats) {
                 Ok(result) => {
                     println!("{}", result)
                 }
                 Err(err) => eprintln!("{}", err),
             };
         } else if args[1] == "--get_random_article" {
-            match storage::get_article_random(&conn) {
+            match storage::get_article_random(&conn, &mut stats) {
                 Ok(result) => {
                     println!("{}", result)
                 }
                 Err(err) => eprintln!("{}", err),
             };
         } else if args[1] == "--article_read" {
-            match storage::remove_last_read_article(&conn) {
+            match storage::remove_last_read_article(&conn, &mut stats) {
                 Ok(_) => {
                     println!("Article removed from list!")
                 }
@@ -54,6 +60,8 @@ fn main() -> Result<()> {
                     eprintln!("{}", err)
                 }
             };
+        } else if args[1] == "--stats" {
+            println!("{:#?}", stats);
         } else {
             print_help();
         }
--- src/storage.rs
+++ src/storage.rs
@@ -1,7 +1,28 @@
+use chrono::{DateTime, Local};
 use rusqlite::{Connection, Result};
-use std::fs::{File, OpenOptions};
+use serde::{Deserialize, Serialize};
+use std::fs::OpenOptions;
 use std::io::{self, Read, Write};
 use std::path::Path;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserStats {
+    articles_in_queue: u32,
+    articles_read: u32,
+    last_article_added_time: DateTime<Local>,
+    last_article_read_time: DateTime<Local>,
+}
+
+impl Default for UserStats {
+    fn default() -> Self {
+        UserStats {
+            articles_in_queue: 0,
+            articles_read: 0,
+            last_article_added_time: Local::now(),
+            last_article_read_time: Local::now(),
+        }
+    }
+}
 
 //I want to use a sqlite database for now to store my items
 pub fn create_tables(conn: &Connection) -> Result<()> {
@@ -18,17 +39,22 @@ pub fn create_tables(conn: &Connection) -> Result<()> {
 }
 
 //Simple initial implementation with simple insertion of item into db
-pub fn add_item(conn: &Connection, article_link: &String) -> Result<()> {
+pub fn add_item(conn: &Connection, article_link: &String, stat_data: &mut UserStats) -> Result<()> {
     let _ = conn.execute(
         "
             insert into reading_items (article_link) values (?1)
         ",
         &[article_link],
     )?;
+
+    (*stat_data).articles_in_queue = (*stat_data).articles_in_queue + 1;
+    (*stat_data).last_article_added_time = Local::now();
+
+    let _ = save_user_stats(stat_data);
     Ok(())
 }
 
-pub fn remove_last_read_article(conn: &Connection) -> Result<()> {
+pub fn remove_last_read_article(conn: &Connection, stat_data: &mut UserStats) -> Result<()> {
     let article_id = match get_last_read_id() {
         Ok(id) => id,
         Err(err) => panic!("{}", err),
@@ -45,20 +71,20 @@ pub fn remove_last_read_article(conn: &Connection) -> Result<()> {
         )?;
     }
 
+    //assign and check for underflow
+    (*stat_data).articles_in_queue = (*stat_data).articles_in_queue - 1;
+    (*stat_data).articles_read = (*stat_data).articles_read + 1;
+
+    if (*stat_data).articles_in_queue == std::u32::MAX {
+        (*stat_data).articles_in_queue = 0
+    }
+
+    let _ = save_user_stats(stat_data);
+
     Ok(())
 }
 
-// pub fn remove_article_with_name(conn: &Connection, article_name: &String) -> Result<()> {
-//     let _ = conn.execute(
-//         "
-//             delete from reading_items where name = (?1)
-//         ",
-//         &[&article_name],
-//     )?;
-//     Ok(())
-// }
-
-pub fn get_article_fifo(conn: &Connection) -> Result<String> {
+pub fn get_article_fifo(conn: &Connection, stat_data: &mut UserStats) -> Result<String> {
     let mut stmt = conn.prepare("SELECT *,min(id) FROM reading_items")?;
 
     let article: Vec<String> = stmt
@@ -80,10 +106,13 @@ pub fn get_article_fifo(conn: &Connection) -> Result<String> {
         }
     };
 
+    (*stat_data).last_article_read_time = Local::now();
+    let _ = save_user_stats(stat_data);
+
     Ok(article[0].clone())
 }
 
-pub fn get_article_random(conn: &Connection) -> Result<String> {
+pub fn get_article_random(conn: &Connection, stat_data: &mut UserStats) -> Result<String> {
     let mut stmt = conn.prepare("SELECT * FROM reading_items ORDER BY RANDOM() LIMIT 1")?;
 
     let article: Vec<String> = stmt
@@ -104,6 +133,9 @@ pub fn get_article_random(conn: &Connection) -> Result<String> {
             println!("{}", err);
         }
     };
+
+    (*stat_data).last_article_read_time = Local::now();
+    let _ = save_user_stats(stat_data);
 
     Ok(article[0].clone())
 }
@@ -162,4 +194,56 @@ fn set_last_read_id(id: i64) -> Result<()> {
     };
 
     Ok(())
+}
+
+fn save_user_stats(user_stats: &UserStats) -> Result<(), io::Error> {
+    const FILE_PATH: &str = "user_stats.json";
+    let path = Path::new(FILE_PATH);
+
+    match OpenOptions::new().write(true).create(true).open(&path) {
+        Ok(mut file) => {
+            // Serialize UserStats to a JSON string
+            let json_data = serde_json::to_string(user_stats)?;
+
+            // Write JSON data to the file
+            file.write_all(json_data.as_bytes())?;
+            Ok(())
+        }
+        Err(err) => Err(err),
+    }
+}
+
+pub fn user_stats() -> Result<UserStats, io::Error> {
+    const FILE_PATH: &str = "user_stats.json";
+
+    let path = Path::new(FILE_PATH);
+
+    match OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(&path)
+    {
+        Ok(mut file) => {
+            let mut contents = String::new();
+            if file.read_to_string(&mut contents)? == 0 {
+                // File is empty, write the default ID
+                let default_stats = UserStats::default();
+                file.write_all(serde_json::to_string(&default_stats)?.as_bytes())?;
+                return Ok(default_stats);
+            }
+
+            match serde_json::from_str(&contents) {
+                Ok(user_stats) => Ok(user_stats),
+                Err(_) => {
+                    // If parsing fails, overwrite with the default ID
+                    let default_stats = UserStats::default();
+                    file.set_len(0)?; // Truncate the file
+                    file.write_all(serde_json::to_string(&default_stats)?.as_bytes())?;
+                    Ok(default_stats)
+                }
+            }
+        }
+        Err(err) => Err(err),
+    }
 }
